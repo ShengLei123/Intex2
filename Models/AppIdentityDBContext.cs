27ded0a22ec62e5afe509ca74a20189fa1758da1
caden-m <90535137+caden-m@users.noreply.github.com>
2022-04-05 15:41:11 -0600

    AppIdentityDBContext

---
+++ Models/AppIdentityDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intex2.Models
+{
+    public class AppIdentityDBContext : IdentityDbContext<IdentityUser>
+    {
+        public AppIdentityDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
